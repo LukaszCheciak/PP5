9f7ac398b74226794edd030e85075397091578ce
Łukasz <s221540@student.uek.krakow.pl>
2023-11-11 19:51:11 +0100

    js

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-var x;
+function zapiszDane() {
+  var nazwa = document.getElementById("nazwa").value;
+  var nrKonta = document.getElementById("NrKonta").value;
+  var nazwaSprzedanejWaluty = document.getElementById(
+    "nazwaSprzedawanejWaluty"
+  ).value;
+  var IloscSprzedanejWaluty = document.getElementById(
+    "IloscSprzedanejWaluty"
+  ).value;
+  var nazwaKupowanejWaluty = document.getElementById(
+    "nazwaKupowanejWaluty"
+  ).value;
+  var akceptacjaPolitykiSerwisu = document.getElementById(
+    "akceptacjaPolitykiSerwisu"
+  ).value;
+
+  var daneObiekt = {
+    nazwa: nazwa,
+    nrKonta: nrKonta,
+    nazwaSprzedanejWaluty: nazwaSprzedanejWaluty,
+    IloscSprzedanejWaluty: IloscSprzedanejWaluty,
+    nazwaKupowanejWaluty: nazwaKupowanejWaluty,
+    akceptacjaPolitykiSerwisu: akceptacjaPolitykiSerwisu,
+  };
+
+  console.log(daneObiekt);
+
+  document.getElementById("sprzedazWalutyFormularz").reset();
+}
+
+fetch("https://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json")
+  .then(function (res) {
+    return res.json();
+  })
+  .then(function (data) {
+    console.log(data);
+  });
