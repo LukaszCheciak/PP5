08ec58b7a4ee5558e7d5749fdcbadaf32d7e19ca
Łukasz <s221540@student.uek.krakow.pl>
2023-11-14 20:15:48 +0100

    forms gone

--- script.js
+++ script.js
@@ -1,20 +1,17 @@
-function zapiszDane() {
-  var nazwa = document.getElementById("nazwa").value;
-  var nrKonta = document.getElementById("NrKonta").value;
-  var nazwaSprzedanejWaluty = document.getElementById(
-    "nazwaSprzedawanejWaluty"
-  ).value;
-  var IloscSprzedanejWaluty = document.getElementById(
-    "IloscSprzedanejWaluty"
-  ).value;
-  var nazwaKupowanejWaluty = document.getElementById(
-    "nazwaKupowanejWaluty"
-  ).value;
-  var akceptacjaPolitykiSerwisu = document.getElementById(
-    "akceptacjaPolitykiSerwisu"
-  ).value;
+function zapiszDane(form) {
+  const nazwa = form.nazwa.value;
+  const nrKonta = form.nrKonta.value;
+  const nazwaSprzedanejWaluty = form.nazwaSprzedawanejWaluty.value;
+  const IloscSprzedanejWaluty = form.iloscSprzedawanejWaluty.value;
+  const nazwaKupowanejWaluty = form.nazwaKupowanejWaluty.value;
+  const akceptacjaPolitykiSerwisu = form.akceptacjaPolitykiSerwisu.checked;
 
-  var daneObiekt = {
+  if (nazwaSprzedanejWaluty === nazwaKupowanejWaluty) {
+    form.reset();
+    return;
+  }
+
+  const daneObiekt = {
     nazwa: nazwa,
     nrKonta: nrKonta,
     nazwaSprzedanejWaluty: nazwaSprzedanejWaluty,
@@ -23,15 +20,75 @@ function zapiszDane() {
     akceptacjaPolitykiSerwisu: akceptacjaPolitykiSerwisu,
   };
 
-  console.log(daneObiekt);
+  handleCalculation(daneObiekt);
 
-  document.getElementById("sprzedazWalutyFormularz").reset();
+  calculate(daneObiekt).then((pr) => console.log(pr));
+
+  form.reset();
 }
 
-fetch("https://api.nbp.pl/api/exchangerates/rates/A/EUR/?format=json")
-  .then(function (res) {
-    return res.json();
-  })
-  .then(function (data) {
-    console.log(data);
-  });
+const handleCalculation = async (daneObiekt) => {
+  const IloscKupowanejWaluty = await calculate(daneObiekt);
+
+  document
+    .getElementById("exchange__result")
+    .insertAdjacentHTML(
+      "beforeend",
+      generateExchangeResult({ ...daneObiekt, IloscKupowanejWaluty })
+    );
+};
+
+const generateExchangeResult = (dane) => {
+  return `
+  <h2>Exchange result #${generateRandomNumber(10000)} </h2>
+  <p>Selling currency: ${dane.nazwaSprzedanejWaluty}</p>
+  <p>Selling value: ${dane.IloscSprzedanejWaluty}</p>
+  <p>Buying currency: ${dane.nazwaKupowanejWaluty}</p>
+  <p>Buying value: ${dane.IloscKupowanejWaluty.toFixed(2)}</p>
+  
+  `;
+};
+
+const generateRandomNumber = (limit) => {
+  return Math.floor(Math.random() * limit) + 1;
+};
+const calculate = async (formData) => {
+  if (formData.nazwaSprzedanejWaluty === "PLN") {
+    const buyCurrencyRate = await fetchCurrencyRate(
+      formData.nazwaKupowanejWaluty
+    );
+    return formData.IloscSprzedanejWaluty / buyCurrencyRate;
+  }
+
+  if (formData.nazwaKupowanejWaluty === "PLN") {
+    const sellCurrencyRate = await fetchCurrencyRate(
+      formData.nazwaSprzedanejWaluty
+    );
+    return formData.IloscSprzedanejWaluty * sellCurrencyRate;
+  }
+
+  const sellCurrencyRate = await fetchCurrencyRate(
+    formData.nazwaSprzedanejWaluty
+  );
+  const buyCurrencyRate = await fetchCurrencyRate(
+    formData.nazwaKupowanejWaluty
+  );
+
+  return (sellCurrencyRate / buyCurrencyRate) * formData.IloscSprzedanejWaluty;
+};
+
+const fetchCurrencyRate = async (currency) => {
+  const response = await fetch(
+    `https://api.nbp.pl/api/exchangerates/rates/A/${currency}/?format=json`
+  );
+  const data = await response.json();
+
+  return data.rates[0].mid;
+};
+
+const form = document.getElementById("sprzedazWalutyFormularz");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  zapiszDane(e.target);
+});
